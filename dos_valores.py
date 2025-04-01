4ea1421cb66bd3de272e22ffcc3bfe3ef3fb5378
aherfue0406 <aherfue0406@g.educaand.es>
2025-04-01 10:06:43 +0200

    3 elementos

--- dos_valores.py
+++ dos_valores.py
@@ -1,9 +1,19 @@
 # Se trata de tomar una lista de dos valores desordenados
 # y producir una salida en orden creciente.
 
-n = [65, 23]
-if n[0] > n[1]:
-    a = n[0]
-    n[0] = n[1]
-    n[1] = a 
-print(n)
+e = [17, 6, 15]
+if e[0] > e[1]:
+    a = e[0]
+    e[0] = e[1]
+    e[1] = a
+
+if e[1] > e[2]:
+   a = e[2]
+   e[1] = e[2]
+   e[2] = a
+
+if e[0] > e[1]:
+   a = e[1]
+   e[0] = e[1]
+   e[1] = a
+print(e)
